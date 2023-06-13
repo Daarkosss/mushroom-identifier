0b46891e8dca869dd567cd3297ea9ed746263c84
Daarkosss <karolpilat59@gmail.com>
2023-06-13 18:44:42 +0200

    Split code into more functions

--- src/model_training/predict_species.py
+++ src/model_training/predict_species.py
@@ -5,31 +5,41 @@ import pickle
 import pathlib
 
 
-def predict_mushroom_species(images_path):
+def predict_mushroom_species(file):
+    img_width, img_height = 224, 224
+    image = tf.keras.preprocessing.image.load_img(
+        file,
+        target_size=(img_height, img_width))
+
+    image_array = tf.keras.utils.img_to_array(image)
+    image_array = tf.expand_dims(image_array, 0)
+
+    predictions = model.predict(image_array)
+    score = tf.nn.softmax(predictions[0])
+
+    species, odds = class_names[np.argmax(score)], np.max(score)
+    return species, odds
+
+
+def predict_mushroom_species_from_dir(images_path):
     testing_files = list(images_path.glob('*.jpg'))
 
     for file in testing_files:
-        img_width, img_height = 224, 224
-        image = tf.keras.preprocessing.image.load_img(
-            file,
-            target_size=(img_height, img_width))
+        species, odds = predict_mushroom_species(file)
+        print_mushroom_species(file, species, odds)
 
-        image_array = tf.keras.utils.img_to_array(image)
-        image_array = tf.expand_dims(image_array, 0)
 
-        predictions = model.predict(image_array)
-        score = tf.nn.softmax(predictions[0])
-
-        print(f'The {file} most likely belongs to '
-              f'{class_names[np.argmax(score)]} with a '
-              f'{100 * np.max(score):.2f} percent confidence.')
+def print_mushroom_species(file, species, odds):
+    print(f'The {file} most likely belongs to '
+          f'{species} with a '
+          f'{100 * odds:.2f} percent confidence.')
 
 
 if __name__ == '__main__':
-    model = load_model('model/Model(ResNet50).h5')
+    model = load_model('prediction_models/Model(ResNet50).h5')
 
     with open('prediction_models/class_names.pkl', 'rb') as file:
         class_names = pickle.load(file)
 
     images_path = pathlib.Path("data/test_images")
-    predict_mushroom_species(images_path)
+    predict_mushroom_species_from_dir(images_path)
