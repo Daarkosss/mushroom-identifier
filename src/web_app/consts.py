2b195a0a71a6b1a9fd2ec3de05108a30168417cf
Daarkosss <karolpilat59@gmail.com>
2023-06-15 14:22:35 +0200

    Add species table

---
+++ src/web_app/consts.py
@@ -0,0 +1,13 @@
+mushroom_species = [
+    ['latin name', 'polish name', 'edibility'],
+    ['amanita muscaria', 'muchomor czerwony', 'poisonous'],
+    ['amanita phalloides', 'muchomor zielonawy', 'deadly poisonous'],
+    ['armillaria mellea', 'opieńka miodowa', 'edible'],
+    ['boletus edulis', 'borowik szlachetny', 'edible'],
+    ['cantharellus cibarius', 'pieprznik jadalny', 'edible'],
+    ['imleria badia', 'podgrzybek brunatny', 'edible'],
+    ['leccinum scabrum', 'koźlarz babka', 'edible'],
+    ['macrolepiota procera', 'czubajka kania', 'edible'],
+    ['suillus luteus', 'maślak zwyczajny', 'edible'],
+    ['tricholoma equestre', 'gąska zielonka', 'edible']
+]
