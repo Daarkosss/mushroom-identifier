61df3b8e8845862dcd0497b6446edc5dc216de76
Daarkosss <karolpilat59@gmail.com>
2023-06-20 20:23:40 +0200

    Code refactor, use json instead of pickle

--- src/model_training/model_resnet50.py
+++ src/model_training/model_resnet50.py
@@ -1,107 +1,139 @@
 # Imports needed
 import tensorflow as tf
-from tensorflow.keras import layers
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 import pathlib
+import os
+import json
 
 
-tf.config.optimizer.set_jit(False)
-dataset_directory = pathlib.Path("data/pre_images")
+def split_data(path_to_dataset):
+    dataset_directory = pathlib.Path(path_to_dataset)
 
-image_count = len(list(dataset_directory.glob('*/*.jpg')))
-print(image_count)
+    image_count = len(list(dataset_directory.glob('*/*.jpg')))
+    print(image_count)
 
-batch_size = 16
-img_height = 224
-img_width = 224
+    batch_size = 16
+    img_height = 224
+    img_width = 224
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    dataset_directory,
-    labels="inferred",
-    label_mode="int",
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    shuffle="True",
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        dataset_directory,
+        labels='inferred',
+        label_mode='int',
+        validation_split=0.2,
+        subset='training',
+        seed=123,
+        shuffle='True',
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    dataset_directory,
-    labels="inferred",
-    label_mode="int",
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    shuffle="True",
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        dataset_directory,
+        labels='inferred',
+        label_mode='int',
+        validation_split=0.2,
+        subset='validation',
+        seed=123,
+        shuffle='True',
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
 
-class_names = train_ds.class_names
-print(class_names)
-
-AUTOTUNE = tf.data.AUTOTUNE
-
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-normalization_layer = layers.Rescaling(1. / 255)
-
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
+    return train_ds, val_ds
 
 
-num_classes = len(class_names)
+def create_callbacks():
+    checkpoint = ModelCheckpoint(
+        'model_resnet50_2.h5',
+        monitor='val_accuracy',
+        verbose=1,
+        save_best_only=True,
+        save_weights_only=False,
+        mode='auto',
+        save_freq='epoch')
 
-resnet_model = ResNet50(weights="imagenet")
+    early = EarlyStopping(
+        monitor='val_accuracy',
+        min_delta=0,
+        patience=40,
+        verbose=1,
+        mode='auto')
 
-last_layer = resnet_model.get_layer("avg_pool")
-resnet_layers = Model(inputs=resnet_model.inputs, outputs=last_layer.output)
+    reduce_lr = ReduceLROnPlateau(
+        monitor='val_accuracy',
+        factor=0.1,
+        mode='max',
+        cooldown=2,
+        patience=2,
+        min_lr=0)
 
-model = Sequential()
-model.add(resnet_layers)
-model.add(Dense(num_classes))
+    return checkpoint, early, reduce_lr
 
-model.layers[0].trainable = False
 
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True),
-    metrics=['accuracy'])
+def create_model_from_resnet(num_classes):
+    resnet_model = ResNet50(weights='imagenet')
 
-model.summary()
+    last_layer = resnet_model.get_layer('avg_pool')
+    resnet_layers = Model(
+        inputs=resnet_model.inputs,
+        outputs=last_layer.output)
 
-checkpoint = ModelCheckpoint(
-    "model_resnet50.h5",
-    monitor='val_accuracy',
-    verbose=1,
-    save_best_only=True,
-    save_weights_only=False,
-    mode='auto',
-    save_freq="epoch")
-early = EarlyStopping(
-    monitor='val_accuracy',
-    min_delta=0,
-    patience=40,
-    verbose=1,
-    mode='auto')
-reduce_lr = ReduceLROnPlateau(
-    monitor='val_accuracy',
-    factor=0.1,
-    mode='max',
-    cooldown=2,
-    patience=2,
-    min_lr=0)
+    model = Sequential()
+    model.add(resnet_layers)
+    model.add(Dense(num_classes))
 
-epochs = 30
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs,
-    callbacks=[checkpoint, early, reduce_lr])
+    model.layers[0].trainable = False
+
+    return model
+
+
+def get_class_names(path_to_dataset):
+    class_names = os.listdir(path_to_dataset)
+    class_names.sort()
+
+    return class_names
+
+
+def save_class_names_to_json(class_names):
+    class_names_dict = {i: name for i, name in enumerate(class_names)}
+
+    with open('prediction_model/class_names.json', 'wb') as f:
+        json.dump(class_names_dict, f)
+
+
+def train_model(path_to_dataset):
+    tf.config.optimizer.set_jit(False)  # Turn off to avoid problems
+
+    train_ds, val_ds = split_data(path_to_dataset)
+
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    class_names = get_class_names(path_to_dataset)
+    save_class_names_to_json(class_names)
+    print(class_names)
+    num_classes = len(class_names)
+
+    model = create_model_from_resnet(num_classes)
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True),
+        metrics=['accuracy'])
+    model.summary()
+
+    checkpoint, early, reduce_lr = create_callbacks()
+    epochs = 30
+    model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        callbacks=[checkpoint, early, reduce_lr])
+
+
+if __name__ == '__main__':
+    train_model('data/pre_images')
--- src/model_training/predict_species.py
+++ src/model_training/predict_species.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
 from keras.models import load_model
 import numpy as np
-import pickle
+import json
 import pathlib
 
 
 def predict_mushroom_species(file):
     model = load_model('prediction_model/model_resnet50.h5')
-    with open('prediction_model/class_names.pkl', 'rb') as class_names_file:
-        class_names = pickle.load(class_names_file)
+
+    with open('prediction_model/class_names.json', 'rb') as class_names_file:
+        class_names = json.load(class_names_file)
+    class_names = {int(k): v for k, v in class_names.items()}
+    print(class_names)
 
     img_width, img_height = 224, 224
     image = tf.keras.preprocessing.image.load_img(
@@ -36,7 +39,7 @@ def predict_mushroom_species_from_dir(images_path):
 def print_mushroom_species(file, species, odds):
     print(f'The {file} most likely belongs to '
           f'{species} with a '
-          f'{100 * odds:.2f} percent confidence.')
+          f'{odds:.2f} percent confidence.')
 
 
 if __name__ == '__main__':
